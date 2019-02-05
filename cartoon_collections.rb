896a1f6a78b9855e192e4753dfccadb5f565f653
vyskoutaris <vyskoutaris@hotmail.com>
2019-02-05 15:10:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,9 +20,9 @@ short_words = ["puff", "go", "two"]
 assorted_words = ["two", "go", "industrious", "bop"]
 
 def long_planeteer_calls(arra)
+  
   if arra.length > 4 ?  true : false
 end
-  
 
 
 def find_the_cheese(array)
