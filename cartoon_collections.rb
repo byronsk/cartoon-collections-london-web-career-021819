ef3dd0d0514d4ce91eb0ca2119f0b6e4b6c57636
vyskoutaris <vyskoutaris@hotmail.com>
2019-02-05 15:08:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ short_words = ["puff", "go", "two"]
 assorted_words = ["two", "go", "industrious", "bop"]
 
 def long_planeteer_calls(arra)
-  if arra.length > 4 ? return true : false
+  if arra.length > 4 ?  true : false
 end
   
 
